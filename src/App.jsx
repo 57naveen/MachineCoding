ea98ce7802cdbb9d2e1255b17c5e935c3dc8683f
57naveen <naveenkishore65@gmail.com>
2025-05-28 11:26:57 +0530

    pagination completed

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import FileExplorer from './Components/File Explorer/FileExplorer'
 import OtpInput from './Components/OTP Input/OtpInput'
 import Pagination from './Components/Pagination/Pagination'
 import ProgressBar from './Components/ProgressBar/ProgressBar'
+import TabForm from './Components/Tab Form Component/TabForm'
 
 function App() {
 
@@ -14,7 +15,8 @@ function App() {
     // <ProgressBar progress={34} />
     // <FileExplorer/>
     // <SearchBar/>
-    <Pagination/>
+    // <Pagination/>
+    <TabForm/>
   )
 }
 
--- src/Components/Pagination/Pagination.jsx
+++ src/Components/Pagination/Pagination.jsx
@@ -1,64 +1,70 @@
-import React, { useEffect, useState } from 'react'
-import Cards from './Cards'
+import React, { useEffect, useState } from "react";
+import Cards from "./Cards";
 
 const Pagination = () => {
-
-  const [results,setResults] = useState([])
-  const [currentPage,setCurrentPage] = useState(0);
+  const [results, setResults] = useState([]);
+  const [currentPage, setCurrentPage] = useState(0);
 
   const PAGE_SIZE = 10;
 
+  const fetchData = async () => {
+    const data = await fetch("https://dummyjson.com/products?limit=500");
+    const json = await data.json();
 
-  const fetchData = async() =>{
+    setResults(json.products);
+  };
 
-    const data = await fetch('https://dummyjson.com/products?limit=500')
-    const json = await data.json()
-
-    setResults(json.products)
-
-  }
-
-
-
-  useEffect(()=>{
-
+  useEffect(() => {
     fetchData();
+  }, []);
 
-  },[])
-
-    const totalProducts = results.length;
-  const noOfPages = Math.ceil(totalProducts/PAGE_SIZE)
+  const totalProducts = results.length;
+  const noOfPages = Math.ceil(totalProducts / PAGE_SIZE);
   const start = currentPage * PAGE_SIZE;
-  const end = start   + PAGE_SIZE
+  const end = start + PAGE_SIZE;
 
-  const handelClick= (n)=>{
+  const handelClick = (n) => {
+    setCurrentPage(n);
+  };
 
-    setCurrentPage(n)
+  const handelNextPage = ( )=>{
+
+    setCurrentPage((prev)=> prev + 1)
 
   }
 
-  return !results.length ? <p>No data found</p> : (
+   const handelPrevPage = ( )=>{
+     setCurrentPage((prev)=> prev - 1)
+  }
+
+
+  return !results.length ? (
+    <p>No data found</p>
+  ) : (
     <div>
+      <h1 className="pagination-title">Pagination</h1>
 
-      <h1 className='pagination-title'>Pagination</h1>
+      <div className="product-container">
+        {results.slice(start, end).map((result) => (
+          <Cards
+            key={result.id}
+            image={result.thumbnail}
+            title={result.title}
+          />
+        ))}
+      </div>
 
-      <div className='product-container'>
-
-     
-      {results.slice(start,end).map((result)=>(
-            <Cards key={result.id} image={result.thumbnail} title={result.title} />
-      ))}
-
-       </div>
-
-       <div className='pagination-container'>
-          {[...Array(noOfPages).keys()].map((n)=>(
-            <span className='page-number' onClick={()=>handelClick(n)} key={n}>{n}</span>
-          ))}
-       </div>
-      
+      <div className="pagination-container">
+        <button  disabled={currentPage === 0} onClick={()=>handelPrevPage()} className="page-number">◀</button>
+        {[...Array(noOfPages).keys()].map((n) => (
+          <button className={"page-number " + ( n === currentPage ? "active-button" : "")} onClick={() => handelClick(n)} key={n}>
+            {n}
+          </button>
+        ))}
+        <button disabled={currentPage === noOfPages -1 } onClick={()=>handelNextPage()} className="page-number">▶</button>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Pagination
+export default Pagination;
